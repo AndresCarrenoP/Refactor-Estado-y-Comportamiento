fdcd67f3c54c0130e01a182698fe3a49c77a286d
AndresCarrenoP <andres.carreno@live.com>
2021-03-13 19:37:37 -0600

    updating Main class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+
+public static void main (String[] args){
+
+    Animal serpiente = new Animal();
+    System.out.print("El animal " + serpiente.nombre + " tiene " + serpiente.piernas + " piernas, y ");
+    serpiente.emitirSonido();
+
+    Animal perro = new Animal("perro", "woof!", "croquetas");
+    System.out.print("El animal " + perro.nombre + " tiene " + perro.piernas + " piernas y come " + perro.comida + ", y ");
+    perro.emitirSonido();
+
+    Animal pato = new Animal("pato", "cua cua");
+    pato.emitirSonido();
+
+    Animal oveja = new Animal("oveja");
+    System.out.print("El animal " + oveja.nombre + " come " + oveja.comida);
+
+
+    }
+
 }
